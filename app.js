de9e9e389721deb20bfded9227307a4fcbe5a7db
shubhamg931 <sg270798@gmail.com>
2018-06-11 04:54:23 +0530

    backend linked

--- app.js
+++ app.js
@@ -5,24 +5,22 @@ const express = require('express'),
     admin     = require('firebase-admin'),
     bodyParser = require('body-parser'),
     serviceAccount = require("./privateKey"),
-    {Pool, Client} = require("pg");
-    require("dotenv").config();
-var path = require('path'),
+    {Pool, Client} = require("pg"),
     multer = require("multer");
 
+require("dotenv").config();
+var path = require('path');
 var dir = path.join(__dirname, 'public');
 app.set("view engine", "ejs");
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
 app.use(express.static(dir));
 
 var connectionString = "postgres://fhebvtlgyvfwcf:a84b41e78b09d13a46684ae447ed3ca961ab6fc7a20f435dd59bdc1c20277138@ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/d998f4888e22o2?ssl=true";
 const pool = new Pool({ connectionString: connectionString});
 
 pool.query("SELECT * from hmm", function (err, result) {
-    if(err) return console.log(err);
+    if(err) 
+        return console.log(err);
     console.log(result.rows);
-    pool.end();
 });
 
 admin.initializeApp({
@@ -38,19 +36,35 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({
-    storage: storage
+    storage: storage,
 });
 
 app.get('/', function(req,res){
     res.render("form");
 });
 
-app.post("/", upload.single("image"), function(req,res){
+app.post("/", upload.single('image'), function(req,res){
     let name = req.body.name;
     console.log(req.body);
+    var x = [];
+    if(typeof req.body.payment == "string")
+        x.push(req.body.payment);
+    else
+        x = req.body.payment;
+
+    var query = {
+        text: 'insert into temp2(name, availability, number, fileName, foodItem, foodPrice, drinkItem, drinkPrice, payment,weekly, monthly, dumping, smartphone, location, startTime, endTime, pin, presence, seating, delivery, dAvailable, route) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22)',
+        values: [req.body.name, req.body.availability, req.body.number, req.body.fileName, req.body.fitem, req.body.fprice, req.body.ditem, req.body.dprice, x,req.body.weekly, req.body.monthly, req.body.dumping, req.body.smartphone, req.body.location, req.body.start, req.body.last, req.body.pin, req.body.presence, req.body.seating, req.body.delivery, req.body.dAvailable, req.body.route],
+    };
+    pool.query(query, function (err, res) {
+        if(err)
+            console.log("ERROR:" + err);
+        else
+            console.log(res.rows[0]);
+    });
     res.render("form");
 });
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 8000, function(){
     console.log("server started!");
 });
 
